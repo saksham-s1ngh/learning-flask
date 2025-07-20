c7dfd5fb266f9430f569208ab9dc235cacf806b9
saksham-s1ngh <saksham249singh@gmail.com>
2025-07-20 12:39:52 -0400

    First step of template rendering with flask Learned how to render templates in flask using the html templates setup in the templates folder, as well as understand directory structure for the same.

--- app.py
+++ app.py
@@ -1,14 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-   return "<h1>Welcome to the homepage!</h1>"
+   return render_template("home.html")
 
 @app.route("/about")
 def about():
-   return """We are a non-profit organization working as an animal rescue. We aim to help you connect with the purrfect furbaby for you! 
-   The animals you find on our website are rescued and rehabilitated animals. Our mission is to promote the ideology \"adopt, don't hop\"! """ 
+   return render_template("about.html")
 
 @app.route("/square/<int:number>")
 def square(number):
@@ -18,4 +17,4 @@ def square(number):
    return f"The square of the {number} is {number**2}"
 
 if __name__ == "__main__":
-    app.run(debug=True, host = "0.0.0.0", port = 3000)
+   app.run(debug=True, host = "0.0.0.0", port = 3000)
