3cfad9a575813955c47b9dd75a734ee76c3030af
saksham-s1ngh <saksham249singh@gmail.com>
2025-07-21 18:14:36 -0400

    (21/07/25)Changed templates to Jinja format Changed the previous HTML templates to be in line with Jinja syntax and structure, created base.html file. Also tried to implement the exercise but was unable to get the images to load in, will do another commit with provided solution to have a record of my solution and the provided one.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 app = Flask(__name__)
+
+"""Information regarding the Pets in the System."""
+pets = [
+            {"id": 1, "name": "Nelly", "age": "5 weeks", "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles."},
+            {"id": 2, "name": "Yuki", "age": "8 months", "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
+            {"id": 3, "name": "Basker", "age": "1 year", "bio": "I love barking. But, I love my friends more."},
+            {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."}, 
+        ]
 
 @app.route("/")
 def home():
-   return render_template("home.html")
+   return render_template("home.html", pets_info=pets)
 
 @app.route("/about")
 def about():
