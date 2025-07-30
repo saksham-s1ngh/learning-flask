f98dbc6ccf974d422491b7bef03f36b4d395f778
saksham-s1ngh <saksham249singh@gmail.com>
2025-07-30 13:43:50 -0400

    (30/07/25)Implemented Dynamic routing Completed the exercise for dynamic routing after understand my errored solution. Still need to fix formatting for the details page.

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, abort
 app = Flask(__name__)
 
 """Information regarding the Pets in the System."""
 pets = [
-            {"id": 1, "name": "Nelly", "age": "5 weeks", "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles."},
-            {"id": 2, "name": "Yuki", "age": "8 months", "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."},
-            {"id": 3, "name": "Basker", "age": "1 year", "bio": "I love barking. But, I love my friends more."},
-            {"id": 4, "name": "Mr. Furrkins", "age": "5 years", "bio": "Probably napping."}, 
+            {
+               "id": 1, 
+               "name": "Nelly", 
+               "age": "5 weeks", 
+               "bio": "I am a tiny kitten rescued by the good people at Paws Rescue Center. I love squeaky toys and cuddles."
+            },
+            {
+               "id": 2, 
+               "name": "Yuki", 
+               "age": "8 months", 
+               "bio": "I am a handsome gentle-cat. I like to dress up in bow ties."
+            },
+            {
+               "id": 3, 
+               "name": "Basker", 
+               "age": "1 year", 
+               "bio": "I love barking. But, I love my friends more."
+            },
+            {
+               "id": 4, 
+               "name": "Mr. Furrkins", 
+               "age": "5 years", 
+               "bio": "Probably napping."
+            }, 
         ]
 
 @app.route("/")
@@ -17,12 +37,22 @@ def home():
 def about():
    return render_template("about.html")
 
-@app.route("/square/<int:number>")
-def square(number):
+# @app.route("/details/<int:number>")
+# def details(number):
+#    """
+#    this route returns the square of the number
+#    """
+#    return f"The square of the {number} is {number**2}"
+
+@app.route("/details/<int:pet_id>")
+def pet_details(pet_id):
    """
-   this route returns the square of the number
+   View function for displaying details of each pet.
    """
-   return f"The square of the {number} is {number**2}"
+   pet = next((pet for pet in pets if pet["id"]==pet_id), None)
+   if pet is None:
+      abort(404, description="No pet was found for the given ID")
+   return render_template("details.html", pet=pet)
 
 if __name__ == "__main__":
    app.run(debug=True, host = "0.0.0.0", port = 3000)
